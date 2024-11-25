fbf336f223282c8f14fa3ce4218bbbbb8d24dd63
cherishma2124 <cherishmayerramada@gmail.com>
2024-11-25 23:59:53 +0530

    final commit

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class prime {
     public static void main(String[] args) {
-        int num = 9;
+        int num = 20;
         boolean isPrime = true;
 
         for (int i = 2; i <= num / 2; i++) {
